ecbb26e468417cc8951cd25b8d2beaf81bcbe445
TAEUNG-NAM <106743597+TAEUNG-NAM@users.noreply.github.com>
2024-02-14 00:29:51 +0900

    게시글 조회 구현
    - 모든 게시글 및 특정 게시글 조회 구현

--- simple-board/src/main/java/com/example/simpleboard/controller/ArticleController.java
+++ simple-board/src/main/java/com/example/simpleboard/controller/ArticleController.java
@@ -7,8 +7,12 @@ import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -17,11 +21,13 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    // 게시글 생성 페이지
     @GetMapping("/articles/new")
     public String newArticle(){
         return "articles/new";
     }
 
+    // 게시글 생성 메소드
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
 
@@ -33,6 +39,32 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
 
-        return "";
+        return "redirect:/articles";
+    }
+
+    // 게시글 조회 메소드
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        // 1. id로 DB에서 Entity를 호출
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 페이지 반환
+        return "articles/show";
+    }
+
+    // 모든 게시글 조회 메소드
+    @GetMapping("/articles")
+    public String main(Model model){
+        // 1. 모든 article을 가져온다
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        // 2. 가져온 article 묶음을 뷰로 전달
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. 페이지 반환
+        return "articles/main";
     }
 }
