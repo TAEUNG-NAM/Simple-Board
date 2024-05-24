c071a46bf17a2b81bb08762adef7da95ce0a7ee6
TAEUNG-NAM <106743597+TAEUNG-NAM@users.noreply.github.com>
2024-05-25 00:52:44 +0900

    OAuth2 로그인(UserDetails, UserDetailsService)
    1. CustomOAuth2User(CustomUserDetailse), OAuth2MemberService(CustomUserDetailsService) 구현
    2. JWTUtil 구현(토큰 검증 및 생성)

---
+++ simple-board/src/main/java/com/example/simpleboard/oauth/CustomOAuth2User.java
@@ -0,0 +1,45 @@
+package com.example.simpleboard.oauth;
+
+import com.example.simpleboard.dto.MemberDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+    private final MemberDto memberDto;
+
+    public CustomOAuth2User(MemberDto memberDto) {
+        this.memberDto = memberDto;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    // 권한리턴
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return memberDto.getRole();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return memberDto.getName();
+    }
+
+    public String getUsername(){
+        return memberDto.getUsername();
+    }
+}
